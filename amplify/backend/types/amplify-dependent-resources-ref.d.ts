da996e21fade09edf7a667b8a3f5b610802b3133
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-05-26 17:10:43 +0100

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "newplayer9315a6ed": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3storify": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
